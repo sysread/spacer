0a5cf818e7d594930dae024bcc0ca94cebe34684
Jeff Ober <sysread@fastmail.fm>
2018-01-23 15:05:08 -0500

    Fix bug showing addons as available when player does not have enough money to purchase

--- www/js/component/addons.js
+++ www/js/component/addons.js
@@ -35,7 +35,7 @@ define(function(require, exports, module) {
       price:        function() { return Math.ceil(this.info.price + (this.info.price * this.place.sales_tax)) },
       sellPrice:    function() { return Math.ceil(0.7 * this.price) },
       isRestricted: function() { return this.info.restricted && !this.player.hasStanding(this.place.faction, this.info.restricted) },
-      canAfford:    function() { return this.player.money < this.price },
+      canAfford:    function() { return this.player.money >= this.price },
       hasRoom:      function() { return this.ship.availableHardPoints() > 0 },
       isAvailable:  function() { return !this.isRestricted && this.canAfford && this.hasRoom },
       hasUpgrade:   function() { return this.ship.hasAddOn(this.type) },
@@ -64,8 +64,8 @@ define(function(require, exports, module) {
   <card v-if="detail" class="my-3" :title="info.name">
     <card-text>{{info.desc}}</card-text>
     <card-text v-if="isRestricted" class="text-warning font-italic">Your reputation with this faction precludes the sale of this equipment to you. That does not prevent you from salivating from the show room window, however.</card-text>
-    <card-text v-if="canAfford" class="text-warning font-italic">You do not have enough money for this upgrade. </card-text>
-    <card-text v-if="hasRoom" class="text-warning font-italic">Your ship does not have enough free hard points for this upgrade.</card-text>
+    <card-text v-if="!canAfford" class="text-warning font-italic">You do not have enough money for this upgrade. </card-text>
+    <card-text v-if="!hasRoom" class="text-warning font-italic">Your ship does not have enough free hard points for this upgrade.</card-text>
 
     <card-text>
       <button v-if="isAvailable" @click="buy=true" type="button" class="btn btn-dark">Purchase</button>
