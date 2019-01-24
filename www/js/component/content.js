3fc3809ad4db7c7f3eaa3183d5b27a861b6c2c5e
Jeff Ober <sysread@fastmail.fm>
2019-01-24 18:18:52 +0000

    Add market-centric debug view

--- www/js/component/content.js
+++ www/js/component/content.js
@@ -58,6 +58,8 @@ define(function(require, exports, module) {
   Vue.component('Testing', {
       data() {
         return {
+          menu: 'bodies',
+          body: window.game.locus,
           item: 'water',
           slow: false,
         };
@@ -73,6 +75,21 @@ define(function(require, exports, module) {
         value: function() {
           if (this.resource) return Math.floor(this.resource.value);
           return 0;
+        },
+
+        minPrice() {
+          if (this.resource) return Math.floor(this.resource.minPrice);
+          return 0;
+        },
+
+        maxPrice() {
+          if (this.resource) return Math.floor(this.resource.maxPrice);
+          return 0;
+        },
+
+        place() {
+          if (this.body)
+            return window.game.planets[this.body];
         },
       },
 
@@ -112,33 +129,56 @@ define(function(require, exports, module) {
       },
 
       template: `
-<card title="Testing">
-  <div class="btn-group">
-    <btn @click="fixMe">Fix me</btn>
-    <btn @click="gameTurns(1)">1 turns</btn>
-    <btn @click="gameTurns(3)">3 turns</btn>
-    <btn @click="gameTurns(9)">9 turns</btn>
-    <btn @click="gameTurns(30)">30 turns</btn>
-    <btn @click="gameTurns(90)">90 turns</btn>
+<card>
+  <div class="input-group input-group-sm my-1">
+    <div class="input-group-prepend btn-group">
+      <btn :disabled="menu == 'items'" @click="menu='items'">Resources</btn>
+      <btn :disabled="menu == 'bodies'" @click="menu='bodies'">Markets</btn>
+    </div>
 
-    <btn v-if="slow" @click="slow=false">Slow</btn>
-    <btn v-else @click="slow=true">Fast</btn>
-  </div>
-
-  <div class="my-3 input-group">
-    <h4 class="mx-1">Resource</h4>
-
-    <select v-model="item" class="form-control mx-1">
-      <option value="">Resource</option>
-      <option v-for="item in resources" :key="item" :value="item">{{item|caps}}</option>
+    <select v-if="menu == 'bodies'" v-model="body" class="form-control">
+      <option value="">Market</option>
+      <option v-for="body in bodies" :key="body" :value="body">{{body|caps}}</option>
     </select>
 
-    <badge>
-      {{value}}c
-    </badge>
+    <select v-if="menu == 'items'" v-model="item" class="form-control">
+      <option value="">Resource</option>
+      <option v-for="item in resources" :key="item" :value="item">
+        {{item|caps}}
+        [ {{value}} c ]
+      </option>
+    </select>
+
+    <span v-if="menu == 'items'">
+      Min: {{ minPrice }}
+      <br />
+      Max: {{ maxPrice }}
+    </span>
   </div>
 
-  <table v-if="item" class="table table-sm mini">
+  <div class="input-group input-group-sm my-1">
+    <div class="input-group-prepend btn-group">
+      <span class="input-group-text">Turns</span>
+      <btn @click="gameTurns(1)">1</btn>
+      <btn @click="gameTurns(3)">3</btn>
+      <btn @click="gameTurns(9)">9</btn>
+      <btn @click="gameTurns(30)">30</btn>
+      <btn @click="gameTurns(90)">90</btn>
+    </div>
+
+    <div class="input-group-prepend btn-group mx-3">
+      <span class="input-group-text">Speed</span>
+      <btn :disabled="slow" @click="slow=true">Slow</btn>
+      <btn :disabled="!slow" @click="slow=false">Fast</btn>
+    </div>
+
+    <div class="input-group-append btn-group">
+      <span class="input-group-text">Misc</span>
+      <btn @click="fixMe">Fix me</btn>
+    </div>
+  </div>
+
+  <table v-if="menu == 'items' && item" class="table table-sm mini">
     <thead>
       <tr>
         <th>Loc</th>
@@ -166,6 +206,36 @@ define(function(require, exports, module) {
       </template>
     </tbody>
   </table>
+
+  <table v-else-if="menu == 'bodies' && body" class="table table-sm mini">
+    <thead>
+      <tr>
+        <th>Item</th>
+        <th class="text-right">Stock</th>
+        <th class="text-right">Price</th>
+        <th class="text-right">Demand</th>
+        <th class="text-right">Supply</th>
+        <th class="text-right">Need</th>
+        <th class="text-right">Net</th>
+        <th class="text-right">Avg</th>
+      </tr>
+    </thead>
+    <tbody>
+      <template v-for="item in resources">
+      <tr :class="{'text-info': place.isNetExporter(item)}">
+        <td>{{item|caps}}</td>
+        <td class="text-right">{{place.getStock(item)}}</td>
+        <td class="text-right">{{place.price(item)|csn}}</td>
+        <td class="text-right">{{place.getDemand(item)|R(2)}}</td>
+        <td class="text-right">{{place.getSupply(item)|R(2)}}</td>
+        <td class="text-right" :class="{'text-success': place.hasSurplus(item), 'text-danger': place.hasShortage(item)}">{{place.getNeed(item)|R(2)}}</td>
+        <td class="text-right">{{place.netProduction(item)|R(2)}}</td>
+        <td class="text-right">{{place.avgProduction(item)|R(2)}}</td>
+      </tr>
+      </template>
+    </tbody>
+  </table>
+
 </card>
       `,
     });
