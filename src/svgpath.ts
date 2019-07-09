c7637ce1f9d130b55a7685fcb4f9bf48064716be
Jeff Ober <jober@ziprecruiter.com>
2019-07-09 15:37:22 -0400

    Simpler implementation of bezier w/o wasm

--- src/svgpath.ts
+++ src/svgpath.ts
@@ -1,28 +1,8 @@
 // adapted from https://medium.com/@francoisromain/smooth-a-svg-path-with-cubic-bezier-curves-e37b49d46c74
 
-declare var wasm: {
-  svgpath: {
-    ctrlpt_x:      (x: number, length: number, angle: number) => number;
-    ctrlpt_y:      (y: number, length: number, angle: number) => number;
-    ctrlpt_length: (px: number, py: number, nx: number, ny: number) => number;
-    ctrlpt_angle:  (px: number, py: number, nx: number, ny: number, reverse: boolean) => number;
-  }
-}
-
 type point = [number, number];
 
-const ctrlpt_x      = wasm.svgpath.ctrlpt_x;
-const ctrlpt_y      = wasm.svgpath.ctrlpt_y;
-const ctrlpt_length = wasm.svgpath.ctrlpt_length;
-const ctrlpt_angle  = wasm.svgpath.ctrlpt_angle;
-
-function control_point(current: point, prev: point, next: point, reverse: boolean): string {
-  const length = ctrlpt_length(prev[0], prev[1], next[0], next[1]);
-  const angle = ctrlpt_angle(prev[0], prev[1], next[0], next[1], reverse);
-  const x = ctrlpt_x(current[0], length, angle);
-  const y = ctrlpt_y(current[1], length, angle);
-  return x + ' ' + y;
-}
+const smoothing = 0.2;
 
 export function bezier(points: point[]): string {
   let path = 'M ' + points[0][0] + ',' + points[0][1];
@@ -33,11 +13,81 @@ export function bezier(points: point[]): string {
     const next    = points[i];
     const nnext   = points[i+1] || current;
 
-    const c1 = control_point(current, prev, next, false);
-    const c2 = control_point(next, current, nnext, true);
+    const l1 = util.length(prev[0], prev[1], next[0], next[1], smoothing);
+    const a1 = util.angle(prev[0], prev[1], next[0], next[1], 0);
+    const x1 = util.ctrlpt_x(current[0], l1, a1);
+    const y1 = util.ctrlpt_y(current[1], l1, a1);
 
-    path += ' C ' + c1 + ',' + c2 + ',' + next[0] + ' ' + next[1];
+    const l2 = util.length(current[0], current[1], nnext[0], nnext[1], smoothing);
+    const a2 = util.angle(current[0], current[1], nnext[0], nnext[1], 1);
+    const x2 = util.ctrlpt_x(next[0], l2, a2);
+    const y2 = util.ctrlpt_y(next[1], l2, a2);
+
+    path += ' C '
+      + x1 + ' ' + y1
+      + ','
+      + x2 + ' ' + y2
+      + ','
+      + next[0] + ' ' + next[1];
   }
 
   return path;
 }
+
+function Util(stdlib: any, foreign: any = null, heap: any = null) {
+  var PI    = stdlib.Math.PI;
+  var hypot = stdlib.Math.hypot;
+  var atan2 = stdlib.Math.atan2;
+  var cos   = stdlib.Math.cos;
+  var sin   = stdlib.Math.sin;
+
+  function length(px: number, py: number, nx: number, ny: number, smoothing: number): number {
+    px = +px;
+    py = +py;
+    nx = +nx;
+    ny = +ny;
+    smoothing = +smoothing;
+    return hypot(nx - px, ny - py) * smoothing;
+  }
+
+  function angle(px: number, py: number, nx: number, ny: number, reverse: number): number {
+    px = +px;
+    py = +py;
+    nx = +nx;
+    ny = +ny;
+    reverse = reverse|0;
+
+    var angle = +0.0;
+
+    angle = atan2(ny - py, nx - px);
+
+    if (reverse == 0) {
+      return angle;
+    } else {
+      return angle + PI;
+    }
+  }
+
+  function ctrlpt_x(x: number, length: number, angle: number): number {
+    x      = +x;
+    length = +length;
+    angle  = +angle;
+    return x + cos(angle) * length;
+  }
+
+  function ctrlpt_y(y: number, length: number, angle: number): number {
+    y      = +y;
+    length = +length;
+    angle  = +angle;
+    return y + sin(angle) * length;
+  }
+
+  return {
+    length:   length,
+    angle:    angle,
+    ctrlpt_x: ctrlpt_x,
+    ctrlpt_y: ctrlpt_y,
+  };
+}
+
+export const util = Util({Math: Math});
--- www/js/svgpath.js
+++ www/js/svgpath.js
@@ -2,17 +2,7 @@
 define(["require", "exports"], function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    const ctrlpt_x = wasm.svgpath.ctrlpt_x;
-    const ctrlpt_y = wasm.svgpath.ctrlpt_y;
-    const ctrlpt_length = wasm.svgpath.ctrlpt_length;
-    const ctrlpt_angle = wasm.svgpath.ctrlpt_angle;
-    function control_point(current, prev, next, reverse) {
-        const length = ctrlpt_length(prev[0], prev[1], next[0], next[1]);
-        const angle = ctrlpt_angle(prev[0], prev[1], next[0], next[1], reverse);
-        const x = ctrlpt_x(current[0], length, angle);
-        const y = ctrlpt_y(current[1], length, angle);
-        return x + ' ' + y;
-    }
+    const smoothing = 0.2;
     function bezier(points) {
         let path = 'M ' + points[0][0] + ',' + points[0][1];
         for (let i = 1; i < points.length; ++i) {
@@ -20,11 +10,71 @@ define(["require", "exports"], function (require, exports) {
             const prev = points[i - 2] || current;
             const next = points[i];
             const nnext = points[i + 1] || current;
-            const c1 = control_point(current, prev, next, false);
-            const c2 = control_point(next, current, nnext, true);
-            path += ' C ' + c1 + ',' + c2 + ',' + next[0] + ' ' + next[1];
+            const l1 = exports.util.length(prev[0], prev[1], next[0], next[1], smoothing);
+            const a1 = exports.util.angle(prev[0], prev[1], next[0], next[1], 0);
+            const x1 = exports.util.ctrlpt_x(current[0], l1, a1);
+            const y1 = exports.util.ctrlpt_y(current[1], l1, a1);
+            const l2 = exports.util.length(current[0], current[1], nnext[0], nnext[1], smoothing);
+            const a2 = exports.util.angle(current[0], current[1], nnext[0], nnext[1], 1);
+            const x2 = exports.util.ctrlpt_x(next[0], l2, a2);
+            const y2 = exports.util.ctrlpt_y(next[1], l2, a2);
+            path += ' C '
+                + x1 + ' ' + y1
+                + ','
+                + x2 + ' ' + y2
+                + ','
+                + next[0] + ' ' + next[1];
         }
         return path;
     }
     exports.bezier = bezier;
+    function Util(stdlib, foreign = null, heap = null) {
+        var PI = stdlib.Math.PI;
+        var hypot = stdlib.Math.hypot;
+        var atan2 = stdlib.Math.atan2;
+        var cos = stdlib.Math.cos;
+        var sin = stdlib.Math.sin;
+        function length(px, py, nx, ny, smoothing) {
+            px = +px;
+            py = +py;
+            nx = +nx;
+            ny = +ny;
+            smoothing = +smoothing;
+            return hypot(nx - px, ny - py) * smoothing;
+        }
+        function angle(px, py, nx, ny, reverse) {
+            px = +px;
+            py = +py;
+            nx = +nx;
+            ny = +ny;
+            reverse = reverse | 0;
+            var angle = +0.0;
+            angle = atan2(ny - py, nx - px);
+            if (reverse == 0) {
+                return angle;
+            }
+            else {
+                return angle + PI;
+            }
+        }
+        function ctrlpt_x(x, length, angle) {
+            x = +x;
+            length = +length;
+            angle = +angle;
+            return x + cos(angle) * length;
+        }
+        function ctrlpt_y(y, length, angle) {
+            y = +y;
+            length = +length;
+            angle = +angle;
+            return y + sin(angle) * length;
+        }
+        return {
+            length: length,
+            angle: angle,
+            ctrlpt_x: ctrlpt_x,
+            ctrlpt_y: ctrlpt_y,
+        };
+    }
+    exports.util = Util({ Math: Math });
 });
